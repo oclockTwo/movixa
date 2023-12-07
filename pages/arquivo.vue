fc373e0087dc53eb2a0d6fd7ad91e07cf17939e8
BingNi <nibing1220@qq.com>
2023-12-08 03:37:56 +0800

    add arquivo

--- pages/arquivo.vue
+++ pages/arquivo.vue
@@ -159,7 +159,7 @@ function getDatesUntilYesterday() {
 }
 
 function gotoDate(date) {
-  window.location.href = `http://movixo.com/arquivo?date=${date}`;
+  window.location.href = `http://movixa.com/arquivo?date=${date}`;
 }
 
 function initData(data) {
