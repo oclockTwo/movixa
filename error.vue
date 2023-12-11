c4c655937ca50ea5f8fa3f6852d62a875a38d3ed
BingNi <nibing1220@qq.com>
2023-12-12 04:51:04 +0800

    add 404 page

--- error.vue
+++ error.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="mt-10">
-    <h1 class="text-center text-4xl">404 - Página não encontrada</h1>
-    <p class="text-center text-xl">Você pode tentar atualizar a página</p>
+  <div class="mt-10 flex flex-col justify-center items-center">
+    <h1 class="text-4xl">404 - Página não encontrada</h1>
+    <p class="text-xl">Você pode tentar atualizar a página</p>
+      <button
+        class="px-7 py-4 mt-10 text-white duration-150 bg-indigo-600 rounded-full hover:bg-indigo-500 active:bg-indigo-700"
+      >
+        <a href="/">voltar para casa</a>
+      </button>
   </div>
 </template>
 
