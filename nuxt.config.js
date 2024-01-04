df268f6a56c7a31e4c751ca4ada6df424217441c
BingNi <nibing1220@qq.com>
2024-01-04 11:23:20 +0800

    add google adsense

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,11 @@ export default defineNuxtConfig({
       script: [
         {
           async: true,
+          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8993913594775765",
+          crossorigin: "anonymous",
+        },
+        {
+          async: true,
           "data-website-id": "df7923f8-c759-4736-9b7d-b57f42d69b5d",
           src: "https://umami-six-theta.vercel.app/script.js",
         },
