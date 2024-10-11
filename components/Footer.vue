aeb6db40a7feb227f26d6bc9222a3597d544360e
Bing Ni <nibing1220@qq.com>
2024-10-11 17:44:15 +0800

    add tomusic.ai link

--- components/Footer.vue
+++ components/Footer.vue
@@ -43,11 +43,11 @@
     <div class="text-sm leading-6 text-center text-neutral-800">
       Copyright © 2023
       <a
-        href="https://movixa.com"
+        href="https://tomusic.ai/"
         target="_self"
         class="leading-6 text-blue-600 cursor-pointer hover:text-blue-700"
         style="text-decoration: underline"
-        >movixa.com</a
+        >Text to Music Generator</a
       >
       All Rights Reserved
     </div>
